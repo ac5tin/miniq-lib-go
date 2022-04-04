4a7f3fabd83c1bcaa029e4e1a8fbf167ed56c4e3
austin <austinchang4@gmail.com>
2022-04-04 19:07:32 +0800

    task struct

---
+++ task.go
@@ -0,0 +1,20 @@
+package main
+
+import "time"
+
+type TaskStatus uint8
+
+const (
+	TaskStatusPending   TaskStatus = iota // just created, waiting to be processed
+	TaskStatusRunning                     // processing by a worker
+	TaskStatusCompleted                   // process failed
+	TaskStatusFailed                      // process completed
+	TaskStatusDelete                      // task is being deleted
+)
+
+type Task struct {
+	ID           string
+	Data         []byte
+	CreationDate time.Time
+	Channel      string
+}
